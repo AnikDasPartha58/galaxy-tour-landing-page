4370873a5c7586b68927084e7f5668dbab56dbc3
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-19 21:14:54 +0600

    finish training page design

---
+++ src/routes/Training.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../assets/Footer';
+import HeroImage from '../components/HeroImage';
+import Navbar from '../components/Navbar';
+import TrainingSec from '../components/Training';
+
+const Training = () => {
+  return (
+    <div>
+        <Navbar/>
+        <HeroImage heading='TRAINING' text='Pre-Flight & In-Flight Training.' />
+        <TrainingSec/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Training
